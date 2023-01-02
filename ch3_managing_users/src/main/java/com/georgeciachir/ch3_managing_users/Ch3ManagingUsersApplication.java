09bb77d582acc8bc6f0e44a1a5370e5ca990891f
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-02 11:41:20 +0200

    add chapter 3 skeleton

---
+++ ch3_managing_users/src/main/java/com/georgeciachir/ch3_managing_users/Ch3ManagingUsersApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch3_managing_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3ManagingUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3ManagingUsersApplication.class, args);
+    }
+
+}
