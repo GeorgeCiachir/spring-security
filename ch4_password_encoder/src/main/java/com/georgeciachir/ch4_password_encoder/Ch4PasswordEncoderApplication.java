b3cc8873cd1f2839a733c596d3e4751ed5a0d52c
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-04 07:12:45 +0200

    add chapter4 skeleton

---
+++ ch4_password_encoder/src/main/java/com/georgeciachir/ch4_password_encoder/Ch4PasswordEncoderApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch4_password_encoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4PasswordEncoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch4PasswordEncoderApplication.class, args);
+    }
+
+}
