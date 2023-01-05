fc59654c446adf47ada172a13938637a4b570b8d
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-05 15:47:35 +0200

    update the service with all the encoders used by Spring

--- ch4_password_encoder/src/main/java/com/georgeciachir/ch4_password_encoder/service/PasswordService.java
+++ ch4_password_encoder/src/main/java/com/georgeciachir/ch4_password_encoder/service/PasswordService.java
@@ -1,11 +1,17 @@
 package com.georgeciachir.ch4_password_encoder.service;
 
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.Md4PasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -13,16 +19,32 @@ public class PasswordService {
 
     private static final String DEFAULT_ENCODER = "bcrypt";
 
-    // Checkout the PasswordEncoderFactories to see the encoders Spring uses
-    private static final Map<String, PasswordEncoder> ENCODERS = Map.of(
-            "noop", NoOpPasswordEncoder.getInstance(),
-            "bcrypt", new BCryptPasswordEncoder(),
-            "scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8()
-    );
+    private static final Map<String, PasswordEncoder> ENCODERS = createEncoders();
 
     public String encode(String password, String encoding) {
         String type = encoding != null ? encoding : DEFAULT_ENCODER;
         PasswordEncoder encoder = ENCODERS.get(type);
         return encoder.encode(password);
+    }
+
+    // Taken from the Spring PasswordEncoderFactories
+    public static Map<String, PasswordEncoder> createEncoders() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("ldap", new LdapShaPasswordEncoder());
+        encoders.put("MD4", new Md4PasswordEncoder());
+        encoders.put("MD5", new MessageDigestPasswordEncoder("MD5"));
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("pbkdf2", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5());
+        encoders.put("pbkdf2@SpringSecurity_v5_8", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+        encoders.put("scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v4_1());
+        encoders.put("scrypt@SpringSecurity_v5_8", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+        encoders.put("SHA-1", new MessageDigestPasswordEncoder("SHA-1"));
+        encoders.put("SHA-256",
+                new MessageDigestPasswordEncoder("SHA-256"));
+        encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
+        encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_2());
+        encoders.put("argon2@SpringSecurity_v5_8", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+        return encoders;
     }
 }
