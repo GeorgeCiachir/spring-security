39bc93a01c3d8ceff420ff12fb87aa2d3263b22e
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-05 14:52:12 +0200

    ch6 skeleton

---
+++ ch6_simple_app/src/main/java/com/georgeciachir/ch6_simple_app/Ch6SimpleAppApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch6_simple_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch6SimpleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch6SimpleAppApplication.class, args);
+    }
+
+}
