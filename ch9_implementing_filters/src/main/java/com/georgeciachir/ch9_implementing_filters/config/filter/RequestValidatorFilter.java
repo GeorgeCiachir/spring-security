ff054ab214ef6970a39b69e7520d2d65d0a6b56b
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-06 08:05:31 +0200

    add some notes

--- ch9_implementing_filters/src/main/java/com/georgeciachir/ch9_implementing_filters/config/filter/RequestValidatorFilter.java
+++ ch9_implementing_filters/src/main/java/com/georgeciachir/ch9_implementing_filters/config/filter/RequestValidatorFilter.java
@@ -12,6 +12,13 @@ import java.io.IOException;
 
 public class RequestValidatorFilter implements Filter {
 
+    // The ExpressionUrlAuthorizationConfigurer has been deprecated, which means that the authorizeRequests() method on the HttpSecurity
+    // object has also been deprecated. The ExpressionUrlAuthorizationConfigurer had the option to permit access based on
+    // an SPEl expression passed to the access(). Of course, this should no longer be used, even if available.
+
+    // In this case, I would say the request id is not the responsibility of a security filter, but foe the sake of the example, is good enough
+
+    // We could use this filter to do what we should no longer do with the ExpressionUrlAuthorizationConfigurer
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
