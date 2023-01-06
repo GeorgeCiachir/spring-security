401c7908bdd6a736efd98ef458d589828596b174
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-06 07:30:35 +0200

    chapter 9 skeleton

---
+++ ch9_implementing_filters/src/main/java/com/georgeciachir/ch9_implementing_filters/Ch9ImplementingFiltersApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch9_implementing_filters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch9ImplementingFiltersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch9ImplementingFiltersApplication.class, args);
+    }
+
+}
