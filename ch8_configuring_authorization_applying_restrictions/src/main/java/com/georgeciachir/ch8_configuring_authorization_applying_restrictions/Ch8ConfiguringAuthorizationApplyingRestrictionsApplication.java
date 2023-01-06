9dbaa60bebc0650b88f20b263438420f0ce4c62c
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-06 07:22:55 +0200

    chapter 8 skeleton

---
+++ ch8_configuring_authorization_applying_restrictions/src/main/java/com/georgeciachir/ch8_configuring_authorization_applying_restrictions/Ch8ConfiguringAuthorizationApplyingRestrictionsApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch8_configuring_authorization_applying_restrictions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch8ConfiguringAuthorizationApplyingRestrictionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch8ConfiguringAuthorizationApplyingRestrictionsApplication.class, args);
+    }
+
+}
