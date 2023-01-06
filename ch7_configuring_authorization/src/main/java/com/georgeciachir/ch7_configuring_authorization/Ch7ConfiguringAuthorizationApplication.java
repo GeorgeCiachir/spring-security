4f20957ef2a3814b46e43bf93e4b79dcf5e2f5d2
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-06 06:29:50 +0200

    chapter 7 skeleton

---
+++ ch7_configuring_authorization/src/main/java/com/georgeciachir/ch7_configuring_authorization/Ch7ConfiguringAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch7_configuring_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch7ConfiguringAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch7ConfiguringAuthorizationApplication.class, args);
+    }
+
+}
