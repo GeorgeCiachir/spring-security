627312e1c51ce06e6aae9fef6cbaa2fe568c74c9
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-07 07:19:41 +0200

    chapter 10 skeleton

---
+++ ch10_applying_csrf_and_cors/src/main/java/com/georgeciachir/ch10_applying_csrf_and_cors/Ch10ApplyingCsrfAndCorsApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch10_applying_csrf_and_cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch10ApplyingCsrfAndCorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch10ApplyingCsrfAndCorsApplication.class, args);
+    }
+
+}
