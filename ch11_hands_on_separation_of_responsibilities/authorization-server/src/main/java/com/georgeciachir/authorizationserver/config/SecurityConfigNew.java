1a9a7f047e320f23ecea3c31fd030d8b6fa917c8
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-09 09:34:14 +0200

    finish the authorization server

---
+++ ch11_hands_on_separation_of_responsibilities/authorization-server/src/main/java/com/georgeciachir/authorizationserver/config/SecurityConfigNew.java
@@ -0,0 +1,28 @@
+package com.georgeciachir.authorizationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigNew {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
---
+++ ch11_hands_on_separation_of_responsibilities/authorization-server/src/main/java/com/georgeciachir/authorizationserver/services/UserService.java
@@ -0,0 +1,66 @@
+package com.georgeciachir.authorizationserver.services;
+
+
+import com.georgeciachir.authorizationserver.entities.Otp;
+import com.georgeciachir.authorizationserver.entities.User;
+import com.georgeciachir.authorizationserver.repositories.OtpRepository;
+import com.georgeciachir.authorizationserver.repositories.UserRepository;
+import com.georgeciachir.authorizationserver.utils.GenerateCodeUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OtpRepository otpRepository;
+
+    public void addUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void auth(User user) {
+        userRepository.findUserByUsername(user.getUsername())
+                .filter(u -> passwordEncoder.matches(user.getPassword(), u.getPassword()))
+                .ifPresentOrElse(this::renewOtp, () -> {
+                    throw new BadCredentialsException("Bad credentials.");
+                });
+    }
+
+    public boolean check(Otp otpToValidate) {
+        return otpRepository.findOtpByUsername(otpToValidate.getUsername())
+                .filter(o -> otpToValidate.getCode().equals(o.getCode()))
+                .isPresent();
+    }
+
+    private void renewOtp(User u) {
+        String code = GenerateCodeUtil.generateCode();
+
+        otpRepository.findOtpByUsername(u.getUsername())
+                .ifPresentOrElse(
+                        o -> updateClientToken(o, code),
+                        () -> createClientToken(u, code));
+    }
+
+    private void createClientToken(User u, String code) {
+        Otp otp = new Otp();
+        otp.setUsername(u.getUsername());
+        otp.setCode(code);
+        otpRepository.save(otp);
+    }
+
+    private void updateClientToken(Otp token, String newValue) {
+        token.setCode(newValue);
+    }
+}
