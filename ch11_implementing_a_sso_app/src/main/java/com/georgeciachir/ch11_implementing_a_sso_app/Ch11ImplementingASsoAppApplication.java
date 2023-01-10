2b65e7e4eaa293d8ee3d2a5c876f6e410bfb86ab
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-10 09:08:46 +0200

    chapter 11 skeleton

---
+++ ch11_implementing_a_sso_app/src/main/java/com/georgeciachir/ch11_implementing_a_sso_app/Ch11ImplementingASsoAppApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch11_implementing_a_sso_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch11ImplementingASsoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch11ImplementingASsoAppApplication.class, args);
+    }
+
+}
