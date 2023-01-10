cc04df3a1c6f1c06f11ba5948cade99ff4c28f12
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-10 11:53:21 +0200

    auth server skeleton

---
+++ ch13_14_15_authotization_and_resource_server/auth_server_oauth/src/main/java/com/georgeciachir/auth_server_oauth/AuthServerOauthApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.auth_server_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerOauthApplication.class, args);
+    }
+
+}
