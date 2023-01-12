48eb9f2431a8f70be652f8ca5d35b43c2d217015
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-12 11:43:51 +0200

    modify the DataSourceAutoconfiguration exclusion

--- ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/TokenEndpointCheckResourceServerConfig.java
+++ ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/TokenEndpointCheckResourceServerConfig.java
@@ -1,12 +1,9 @@
 package com.georgeciachir.resource_server_oauth.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @Profile("token-endpoint-config")
 @EnableResourceServer
 @Configuration
