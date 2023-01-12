39ab9f06562ef432b051d77eca366420c0d050f2
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-12 11:33:09 +0200

    introduce jdbcTokenStore and create different configs based on profiles

--- ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/TokenEndpointCheckResourceServerConfig.java
+++ ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/TokenEndpointCheckResourceServerConfig.java
@@ -1,11 +1,14 @@
 package com.georgeciachir.resource_server_oauth.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@Profile("check-token-endpoint")
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@Profile("token-endpoint-config")
 @EnableResourceServer
 @Configuration
-public class ResourceServerConfig {
+public class TokenEndpointCheckResourceServerConfig {
 }
---
+++ ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/TokenStoreResourceServerConfig.java
@@ -0,0 +1,34 @@
+package com.georgeciachir.resource_server_oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+@Profile("token-store-config")
+@EnableResourceServer
+@Configuration
+public class TokenStoreResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+}
