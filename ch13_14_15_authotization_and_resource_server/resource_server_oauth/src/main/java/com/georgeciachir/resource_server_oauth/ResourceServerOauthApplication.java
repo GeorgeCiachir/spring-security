a35a3b3dbc6dd5ad6db8650c8a0fe2772e249451
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-12 07:14:41 +0200

    resource server skeleton

---
+++ ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/ResourceServerOauthApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.resource_server_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerOauthApplication.class, args);
+    }
+
+}
