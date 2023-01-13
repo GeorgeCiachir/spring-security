d98a7e50cf0f5ce24632dd4e728bb4fee3fea9e9
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-13 09:15:30 +0200

    extract registered clients

--- ch13_14_15_authotization_and_resource_server/auth_server_oauth/src/main/java/com/georgeciachir/auth_server_oauth/config/AuthServerConfig.java
+++ ch13_14_15_authotization_and_resource_server/auth_server_oauth/src/main/java/com/georgeciachir/auth_server_oauth/config/AuthServerConfig.java
@@ -9,14 +9,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @Profile("jdbc-token-store")
 @Configuration
@@ -29,6 +25,9 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private InMemoryRegisteredClients inMemoryClients;
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.authenticationManager(authenticationManager)
@@ -40,16 +39,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
         // Of course, in a real world scenario, this would most likely be a service that
         // retrieves the client details from a DB
         InMemoryClientDetailsService clientDetailsService = new InMemoryClientDetailsService();
-
-        BaseClientDetails passwordGrantTypeClient = passwordGrantTypeClient();
-        BaseClientDetails codeGrantTypeClient = codeGrantTypeClient();
-        BaseClientDetails resourceServerClient = resourceServerClient();
-
-        clientDetailsService.setClientDetailsStore(
-                Map.of(passwordGrantTypeClient.getClientId(), passwordGrantTypeClient,
-                        codeGrantTypeClient.getClientId(), codeGrantTypeClient,
-                        resourceServerClient.getClientId(), resourceServerClient));
-
+        clientDetailsService.setClientDetailsStore(inMemoryClients.get());
         clients.withClientDetails(clientDetailsService);
     }
 
@@ -62,41 +52,5 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
         // If we decide to use authentication for this endpoint, which most likely we should,
         // we also need to declare the resource server as a client for the authorization server
         security.checkTokenAccess("isAuthenticated()");
-    }
-
-    /**
-     * For the authorization server, the resource server is a client in the sense of a user that
-     * needs some info (check if the token is valid) and it is not involved in the token issuing process.
-     * <p>
-     * I could have even removed the need for declaring this client details, but then I would need to add the
-     * "permitAll()" level of authorization on the "/oauth/check_token" endpoint
-     * in the {@link #configure(AuthorizationServerSecurityConfigurer)}
-     */
-    private BaseClientDetails resourceServerClient() {
-        BaseClientDetails clientDetails = new BaseClientDetails();
-        clientDetails.setClientId("resourceServer");
-        clientDetails.setClientSecret("resourceServerSecret");
-        return clientDetails;
-    }
-
-    private BaseClientDetails passwordGrantTypeClient() {
-        BaseClientDetails clientDetails = new BaseClientDetails();
-        clientDetails.setClientId("client_password_grant");
-        clientDetails.setClientSecret("secret");
-        clientDetails.setScope(List.of("read"));
-        // adding the refresh_token grant type ensure that a refresh token is also provided with the default auth token
-        clientDetails.setAuthorizedGrantTypes(List.of("password", "refresh_token"));
-        return clientDetails;
-    }
-
-    private BaseClientDetails codeGrantTypeClient() {
-        BaseClientDetails clientDetails = new BaseClientDetails();
-        clientDetails.setClientId("client_code_grant");
-        clientDetails.setClientSecret("secret");
-        clientDetails.setScope(List.of("read"));
-        // adding the refresh_token grant type ensure that a refresh token is also provided with the default auth token
-        clientDetails.setAuthorizedGrantTypes(List.of("authorization_code", "refresh_token"));
-        clientDetails.setRegisteredRedirectUri(Set.of("http://localhost:8080"));
-        return clientDetails;
     }
 }
---
+++ ch13_14_15_authotization_and_resource_server/auth_server_oauth/src/main/java/com/georgeciachir/auth_server_oauth/config/InMemoryRegisteredClients.java
@@ -0,0 +1,67 @@
+package com.georgeciachir.auth_server_oauth.config;
+
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class InMemoryRegisteredClients {
+
+    private Map<String, BaseClientDetails> clients;
+
+    @PostConstruct
+    public void setUp() {
+        BaseClientDetails passwordGrantTypeClient = passwordGrantTypeClient();
+        BaseClientDetails codeGrantTypeClient = codeGrantTypeClient();
+        BaseClientDetails resourceServerClient = resourceServerClient();
+
+        clients = Map.of(passwordGrantTypeClient.getClientId(), passwordGrantTypeClient,
+                codeGrantTypeClient.getClientId(), codeGrantTypeClient,
+                resourceServerClient.getClientId(), resourceServerClient);
+    }
+
+    public Map<String, BaseClientDetails> get() {
+        return clients;
+    }
+
+    /**
+     * For the authorization server, the resource server is a client in the sense of a user that
+     * needs some info (check if the token is valid) and it is not involved in the token issuing process.
+     * <p>
+     * I could have even removed the need for declaring this client details, but then I would need to add the
+     * "permitAll()" level of authorization on the "/oauth/check_token" endpoint
+     * in the {@link AuthServerConfig#configure(AuthorizationServerSecurityConfigurer)}
+     */
+    private BaseClientDetails resourceServerClient() {
+        BaseClientDetails clientDetails = new BaseClientDetails();
+        clientDetails.setClientId("resourceServer");
+        clientDetails.setClientSecret("resourceServerSecret");
+        return clientDetails;
+    }
+
+    private BaseClientDetails passwordGrantTypeClient() {
+        BaseClientDetails clientDetails = new BaseClientDetails();
+        clientDetails.setClientId("client_password_grant");
+        clientDetails.setClientSecret("secret");
+        clientDetails.setScope(List.of("read"));
+        // adding the refresh_token grant type ensure that a refresh token is also provided with the default auth token
+        clientDetails.setAuthorizedGrantTypes(List.of("password", "refresh_token"));
+        return clientDetails;
+    }
+
+    private BaseClientDetails codeGrantTypeClient() {
+        BaseClientDetails clientDetails = new BaseClientDetails();
+        clientDetails.setClientId("client_code_grant");
+        clientDetails.setClientSecret("secret");
+        clientDetails.setScope(List.of("read"));
+        // adding the refresh_token grant type ensure that a refresh token is also provided with the default auth token
+        clientDetails.setAuthorizedGrantTypes(List.of("authorization_code", "refresh_token"));
+        clientDetails.setRegisteredRedirectUri(Set.of("http://localhost:8080"));
+        return clientDetails;
+    }
+}
