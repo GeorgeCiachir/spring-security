b81a3dc731a78a6d61adfec1feca34498cb96070
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-13 09:05:53 +0200

    removed unused dependencies

--- ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/TokenStoreResourceServerConfig.java
+++ ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/TokenStoreResourceServerConfig.java
@@ -1,8 +1,6 @@
 package com.georgeciachir.resource_server_oauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
