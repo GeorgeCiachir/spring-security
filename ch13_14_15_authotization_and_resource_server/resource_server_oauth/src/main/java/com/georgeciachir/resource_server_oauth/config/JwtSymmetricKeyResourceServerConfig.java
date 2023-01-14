5a24f51e5c0dd9107c789ee6edf4d8d1c6b023f3
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-14 07:16:28 +0200

    add symmetric key signed jwt on the resource server

---
+++ ch13_14_15_authotization_and_resource_server/resource_server_oauth/src/main/java/com/georgeciachir/resource_server_oauth/config/JwtSymmetricKeyResourceServerConfig.java
@@ -0,0 +1,38 @@
+package com.georgeciachir.resource_server_oauth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Profile("jwt-symmetric-key-token-store")
+@Configuration
+@EnableResourceServer
+public class JwtSymmetricKeyResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${jwt.symmetric.key}")
+    private String jwtSymmetricKey;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtSymmetricKey);
+        return converter;
+    }
+}
