a7a9c346228386855a1bc3732b3daa670cb2d5d5
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-17 08:33:22 +0200

    global method security skeleton

---
+++ ch16_17_global_method_security/src/main/java/com/georgeciachir/ch16_17_global_method_security/Ch1617GlobalMethodSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch16_17_global_method_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch1617GlobalMethodSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch1617GlobalMethodSecurityApplication.class, args);
+    }
+
+}
