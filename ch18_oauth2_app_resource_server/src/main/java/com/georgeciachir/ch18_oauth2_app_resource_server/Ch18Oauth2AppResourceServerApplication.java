43c753ff73c51400bc09091f81c25bdda7f05904
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-19 16:18:51 +0200

    ch 18 skeleton

---
+++ ch18_oauth2_app_resource_server/src/main/java/com/georgeciachir/ch18_oauth2_app_resource_server/Ch18Oauth2AppResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.ch18_oauth2_app_resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch18Oauth2AppResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch18Oauth2AppResourceServerApplication.class, args);
+    }
+
+}
