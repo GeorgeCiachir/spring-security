c1b2913a22cad11a432038d34644773ab643f94e
GeorgeCiachir <george.ciachir@yahoo.com>
2023-01-23 06:22:52 +0200

    refactor config

--- ch18_oauth2_app_resource_server/src/main/java/com/georgeciachir/ch18_oauth2_app_resource_server/config/ResourceServerConfig.java
+++ ch18_oauth2_app_resource_server/src/main/java/com/georgeciachir/ch18_oauth2_app_resource_server/config/ResourceServerConfig.java
@@ -1,48 +1,68 @@
 package com.georgeciachir.ch18_oauth2_app_resource_server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimValidator;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+import java.util.List;
+
+import static org.springframework.security.oauth2.jwt.JwtClaimNames.AUD;
+
+@EnableMethodSecurity
 @Configuration
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig {
 
     @Value("${claim.aud}")
     private String claimAudience;
 
+    @Value("${keycloak.base.url}")
+    private String keycloakBaseUrl;
+
     @Value("${jwk.key-uri}")
     private String urlJwk;
 
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.tokenStore(tokenStore());
-        resources.resourceId(claimAudience);
-    }
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .mvcMatchers(HttpMethod.DELETE, "/**")
-                .hasAuthority("imperial-admin")
-                .anyRequest().permitAll();
-    }
-
-    // We could have also set up a JwtTokenStore and read the public key from the resources folder
-    // See the JwtAsymmetricKeyAuthServerConfig class in the ch13_14_15_authorization_and_resource_server project
     @Bean
-    public TokenStore tokenStore() {
-        return new JwkTokenStore(urlJwk);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.oauth2Login()
+                .clientRegistrationRepository(clientRegistrationRepository)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.DELETE, "/**").hasAuthority("imperial-admin")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer(rsc -> rsc.jwt(jwt -> jwt.jwkSetUri(urlJwk).decoder(jwtDecoder())))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        OAuth2TokenValidator<Jwt> audienceValidator = new DelegatingOAuth2TokenValidator<>(audienceValidator());
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(keycloakBaseUrl);
+        jwtDecoder.setJwtValidator(audienceValidator);
+
+        return jwtDecoder;
+    }
+
+    @Bean
+    public OAuth2TokenValidator<Jwt> audienceValidator() {
+        return new JwtClaimValidator<List<String>>(AUD, aud -> aud.contains(claimAudience));
     }
 
     // Required so that the expression used at the ProductRepository can be evaluated
