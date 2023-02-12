3df1faba2ed18467f75de302a41cc5a2f87eb218
GeorgeCiachir <george.ciachir@yahoo.com>
2023-02-12 08:11:12 +0200

    add client skeleton

---
+++ complete_setup/resource_server_client/src/main/java/com/georgeciachir/resource_server_client/ResourceServerClientApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.resource_server_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerClientApplication.class, args);
+    }
+
+}
