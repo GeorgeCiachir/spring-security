a401aaab933c573eadf7e05c7164f3ae963dcbf9
GeorgeCiachir <george.ciachir@yahoo.com>
2023-02-18 15:30:28 +0200

    register the client with the auth server

--- complete_setup/client/src/main/java/com/georgeciachir/client/config/AppRegistrations.java
+++ complete_setup/client/src/main/java/com/georgeciachir/client/config/AppRegistrations.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 
 import java.util.List;
 
@@ -25,11 +26,18 @@ public class AppRegistrations {
     @Value("${security.keycloak.client.registration.client-secret}")
     private String keycloakClientSecret;
 
+    @Value("${security.spring.oauth2.client.registration.client-id}")
+    private String springOAuth2ClientId;
+
+    @Value("${security.spring.oauth2.client.registration.client-secret}")
+    private String springOAuth2ClientSecret;
+
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
         List<ClientRegistration> clientRegistrations = List.of(
                 auth0ClientRegistration(),
-                keycloakClientRegistration());
+                keycloakClientRegistration(),
+                springOAuth2ClientRegistration());
         return new InMemoryClientRegistrationRepository(clientRegistrations);
     }
 
@@ -37,7 +45,7 @@ public class AppRegistrations {
         return ClientRegistration.withRegistrationId("auth0") // whatever id you want. just has to be unique
                 .clientId(auth0ClientId)
                 .clientSecret(auth0ClientSecret)
-                .scope("openid", "profile")
+                .scope(OidcScopes.OPENID, OidcScopes.PROFILE)
                 .authorizationUri("https://product-management-backend.eu.auth0.com/authorize?audience=product-management-backend")
                 .tokenUri("https://product-management-backend.eu.auth0.com/oauth/token")
                 .userInfoUri("https://product-management-backend.eu.auth0.com/userinfo")
@@ -51,12 +59,11 @@ public class AppRegistrations {
                 .build();
     }
 
-
     private ClientRegistration keycloakClientRegistration() {
         return ClientRegistration.withRegistrationId("keycloak") // whatever id you want. just has to be unique
                 .clientId(keycloakClientId)
                 .clientSecret(keycloakClientSecret)
-                .scope("product-management-backend", "openid", "profile")
+                .scope("product-management-backend", OidcScopes.OPENID, OidcScopes.PROFILE)
                 .authorizationUri("http://localhost:8080/auth/realms/online-shopping-products-management/protocol/openid-connect/auth")
                 .tokenUri("http://localhost:8080/auth/realms/online-shopping-products-management/protocol/openid-connect/token")
                 .userInfoUri("http://localhost:8080/auth/realms/online-shopping-products-management/protocol/openid-connect/userinfo")
@@ -67,6 +74,38 @@ public class AppRegistrations {
                 .clientName("Keycloak")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUri("{baseUrl}/{action}/oauth2/code/{registrationId}")
+                .build();
+    }
+
+    // Use http://127.0.0.1:9090/hello as an entry point, instead of http://localhost/hello
+
+    // Need to enter the resource server using the IP literal (127.0.0.1) due to how the
+    // spring oauth2 server is implemented. In the authorization server see this method that validates
+    // the redirect_uri:
+    // OAuth2AuthorizationCodeRequestAuthenticationValidator::validateRedirectUri
+    // The use of "localhost" is NOT RECOMMENDED -> it actually throws OAuth2Error
+
+    // Because the auth server does not accept "localhost" as a redirect_uri I need to use 127.0.0.1. The
+    // session id that is provided along with the authorization code on the redirect_uri, is then
+    // stored in the browser on 127.0.0.1 and does not apply to localhost. This means that when the resource
+    // server redirects the user on the initial requested uri (localhost:9090/hello) after a successful login,
+    // an exception will be thrown due to the cookie missing in the browser for localhost.
+
+    private ClientRegistration springOAuth2ClientRegistration() {
+        return ClientRegistration.withRegistrationId("spring-auth") // whatever id you want. just has to be unique
+                .clientId(springOAuth2ClientId)
+                .clientSecret(springOAuth2ClientSecret)
+                .scope(OidcScopes.OPENID, OidcScopes.PROFILE)
+                .authorizationUri("http://localhost:9092/oauth2/authorize")
+                .tokenUri("http://localhost:9092/oauth2/token")
+                .userInfoUri("http://localhost:9092/userinfo")
+                // Required for signature verification in case the userinfo endpoint is not used -> in case of openid scope
+                .jwkSetUri("http://localhost:9092/oauth2/jwks")
+                // This has been manually added to the claims in the auth server config
+                .userNameAttributeName("sub")
+                .clientName("Spring OAuth2")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://127.0.0.1:9091/{action}/oauth2/code/{registrationId}")
                 .build();
     }
 }
